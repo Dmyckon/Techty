f521fcabfb22e791e184614f092b187624ffc6c1
Dmyckon <dmyckon@gmail.com>
2025-04-17 15:03:53 +0100

    add hero section with functions

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function menuView() {
+    document.getElementById("menu").style.display = "block"
+    document.getElementById("logo").style.display = "none"
+    document.getElementById("butt").style.display = "none"
+    document.getElementById("harm").style.display = "none"
+    document.getElementById("ex").style.display = "block"
+}
+
+function cancelView() {
+    document.getElementById("menu").style.display = "none"
+    document.getElementById("logo").style.display = "block"
+    document.getElementById("butt").style.display = "block"
+    document.getElementById("harm").style.display = "block"
+    document.getElementById("ex").style.display = "none"
+}
