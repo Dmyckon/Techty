1bf3e788f9cd3e78e54baa68aba26cb31490c6e2
Dmyckon <dmyckon@gmail.com>
2025-04-22 15:22:23 +0100

    add subsection

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function cancelView() {
     document.getElementById("harm").style.display = "block"
     document.getElementById("ex").style.display = "none"
 }
+
+
+var testimony = [document.getElementById("one").innerHTML, document.getElementById("two").innerHTML, document.getElementById("three").innerHTML, document.getElementById("four").innerHTML, document.getElementById("five").innerHTML, document.getElementById("six").innerHTML
+]
+var index = 0
+var index = 5
+
+function nextClick() {
+    index++
+            if (index >= testimony.length) {
+                index = 0
+            }
+            document.getElementById("one").innerHTML = testimony[index]
+}
+
+function prevClick() {
+    index--
+    if (index >= testimony.length) {
+        index = 5
+    }
+    document.getElementById("one").innerHTML = testimony[index]
+}
+
