945a04eaba5769f3728d7384102ae0cf739164c3
Dmyckon <dmyckon@gmail.com>
2025-04-24 12:26:00 +0100

    add about section to the webpage

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ function cancelView() {
     document.getElementById("ex").style.display = "none"
 }
 
+function menuView2() {
+    document.getElementById("menu2").style.display = "block"
+    document.getElementById("logo2").style.display = "none"
+    document.getElementById("butt2").style.display = "none"
+    document.getElementById("harm2").style.display = "none"
+    document.getElementById("ex2").style.display = "block"
+}
+
+function cancelView2() {
+    document.getElementById("menu2").style.display = "none"
+    document.getElementById("logo2").style.display = "block"
+    document.getElementById("butt2").style.display = "block"
+    document.getElementById("harm2").style.display = "block"
+    document.getElementById("ex2").style.display = "none"
+}
+
 
 var testimony = [document.getElementById("one").innerHTML, document.getElementById("two").innerHTML, document.getElementById("three").innerHTML, document.getElementById("four").innerHTML, document.getElementById("five").innerHTML, document.getElementById("six").innerHTML
 ]
@@ -36,5 +52,24 @@ function prevClick() {
         index = 5
     }
     document.getElementById("one").innerHTML = testimony[index]
+}
+
+var team = [document.getElementById("image1").innerHTML, document.getElementById("image2").innerHTML, document.getElementById("image2").innerHTML, document.getElementById("image4").innerHTML
+]
+
+function nextClick2() {
+    index++
+            if (index >= team.length) {
+                index = 0
+            }
+            document.getElementById("image1").innerHTML = team[index]
+}
+
+function prevClick2() {
+    index--
+    if (index >= team.length) {
+        index = 5
+    }
+    document.getElementById("image1").innerHTML = team[index]
 }
 
